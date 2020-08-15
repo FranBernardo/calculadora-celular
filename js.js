ea97ce96d701b85bccaae48cbd269f7a76b3a025
Fran Bernardo <francielebernardo88@hotmail.com>
2020-08-15 16:21:49 -0300

    retirando o bootstrap

--- js.js
+++ js.js
@@ -1,13 +1,17 @@
-var valor;
-var resultado;
+// testa a forma diferente e testando a propriedade eval
+
+let valor;
+let resultado;
 function botao(num){
     valor= document.calc.visor.value += num;
 } 
 
 function resete(){
-    document.calc.visor.value = '';
+    document.calc.visor.value = ''; // UM jeito simplificado
 }
 function calcular(){
-    resultado = eval(valor);
+    resultado = eval(valor); // eval é bem melhor para usar em calculos
     document.calc.visor.value = resultado;
 }
+
+
